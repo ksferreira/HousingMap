bc25c9b21759167374f30d34f6e0579e6d6965cc
Kauan Ferreira <ksferreira11@gmail.com>
2025-05-06 23:54:24 -0400

    Utilize sqlite3 database over unimplemented InPlaceMemoryModel

---
+++ backend/src/model/ModelFactory.js
@@ -0,0 +1,35 @@
+// import InMemoryTaskModel from "./InMemoryTaskModel.js";
+import SQLiteTaskModel from "./SQLiteTaskModel.js";
+import TownStatsModel from "./TownStatsModel.js";
+
+class _ModelFactory {
+  async getModel(model = "sqlite") {
+    if (model === "sqlite") {
+      return SQLiteTaskModel;
+    } else if (model === "sqlite-fresh") {
+      await SQLiteTaskModel.init(true);
+      try {
+        await TownStatsModel.init();
+      } catch (error) {
+        console.error('Error initializing TownStatsModel:', error);
+        throw error;
+      }
+      return SQLiteTaskModel;
+    } else {
+      return InMemoryTaskModel;
+    }
+  }
+
+  async getTownStatsModel() {
+    try {
+      await TownStatsModel.init();
+      return TownStatsModel;
+    } catch (error) {
+      console.error('Error initializing TownStatsModel:', error);
+      throw error;
+    }
+  }
+}
+
+const ModelFactory = new _ModelFactory();
+export default ModelFactory;
