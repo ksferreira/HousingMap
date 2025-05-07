761c069289487389bdd0f48b105da0772022961b
Kauan Ferreira <ksferreira11@gmail.com>
2025-05-06 23:59:01 -0400

    Moved to the root folder of the backend

---
+++ backend/Server.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import StatRoutes from './src/routes/StatRoutes.js';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const rootDir = join(__dirname, '..');
+
+class Server {
+    constructor() {
+        this.app = express();
+        this.configureMiddleware();
+        this.setupRoutes();
+    }
+
+    configureMiddleware() {
+        this.app.use(express.static(join(rootDir, 'frontend/dist')))
+        this.app.use(express.json({ limit: '10mb' }));
+    }
+
+    setupRoutes() {
+        this.app.use('/v1', StatRoutes);
+        
+        // fallback route
+        this.app.get('*', (req, res) => {
+            res.sendFile(join(rootDir, 'frontend/dist/index.html'));
+        });
+    }
+
+    start(port = 3000) {
+        this.app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+    }
+}
+
+
+console.log("Starting server...");
+const server = new Server();
+server.start();
