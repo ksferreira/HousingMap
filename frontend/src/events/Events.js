ecd5c07bfce09bc90be983c0f26a243aaa8de591
Kauan Ferreira <ksferreira11@gmail.com>
2025-05-07 23:04:30 -0400

    Removed template event names, added relevant names for location searc/success/failure

--- frontend/src/events/Events.js
+++ frontend/src/events/Events.js
@@ -1,15 +1,5 @@
 export const Events = {
-    Newfav: 'NewFAV',
-
-    LoadFav: 'LoadFav',
-    LoadFavSuccess: 'LoadFavSuccess',
-    LoadFavFailure: 'LoadFavFailure',
-  
-    StoreFav: 'StoreFav',
-    StoreFavSuccess: 'StoreFavSuccess',
-    StoreFavFailure: 'StoreFavFailure',
-  
-    UnStoreFav: 'UnStoreFav',
-    UnStoreFavSuccess: 'UnStoreFavSuccess',
-    UnStoreFavFailure: 'UnStoreFavFailure',
+    SearchLocation: 'SearchLocation',
+    SearchLocationSuccess: 'SearchLocationSuccess',
+    SearchLocationFailure: 'SearchLocationFailure',
 };
